263bfaecfa8a100f12b14ab0f77f8d6408bda352
Suleman Hafeez <sulemanhafeezmuhammad@gmail.com>
2025-10-05 10:46:49 +0500

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { setupVite, log } from "./vite";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 app.use(express.json());
@@ -35,6 +38,23 @@ app.use((req, res, next) => {
 
   next();
 });
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function serveStatic(app2: express.Application) {
+  const distPath = path.resolve(__dirname, "public"); // Agar public folder dist ke andar hai
+  // Agar public folder dist/public ke andar hai, toh:
+  // const distPath = path.resolve(__dirname, "dist", "public");
+  if (!fs.existsSync(distPath)) {
+    throw new Error(
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
+    );
+  }
+  app2.use(express.static(distPath));
+  app2.use("*", (_req, res) => {
+    res.sendFile(path.resolve(distPath, "index.html"));
+  });
+}
 
 (async () => {
   const server = await registerRoutes(app);
